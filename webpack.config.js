73d90901ea2f7edde19e350ce49d6740e766f18f
Lukas Winkler <git@lw1.at>
2019-04-22 17:32:28 +0200

    dependency update

--- webpack.config.js
+++ webpack.config.js
@@ -121,7 +121,7 @@ if (process.env.NODE_ENV === "production") {
     module.exports.devtool = "#source-map";
     // http://vue-loader.vuejs.org/en/workflow/production.html
     module.exports.plugins = (module.exports.plugins || []).concat([
-        new CleanWebpackPlugin("dist"),
+        new CleanWebpackPlugin(),
         new CopyWebpackPlugin([{from: "icon", to: "icon"}]),
         new webpack.HashedModuleIdsPlugin({
             hashFunction: "sha256",
