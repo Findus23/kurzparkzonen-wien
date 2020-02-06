276932c5b335816a3a470524ef3861a44a36eeae
Lukas Winkler <git@lw1.at>
2020-02-06 21:20:33 +0100

    fix sentry

--- android/app/src/main/java/at/lw1/kurzparkzonen/wien/MainActivity.java
+++ android/app/src/main/java/at/lw1/kurzparkzonen/wien/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             Context ctx = this.getApplicationContext();
 
             // Use the Sentry DSN (client key) from the Project Settings page on Sentry
-            String sentryDsn = "https://31e14ecca4964ecaa8debcc38357f87e@sentry.lw1.at/8";
+            String sentryDsn = "https://a466af869d1942d4b70b60e798c5e234@sentry.lw1.at/10";
             Sentry.init(sentryDsn, new AndroidSentryClientFactory(ctx));
         } else {
             Log.i("Kurzparkzonen", "crash reports are disabled");
